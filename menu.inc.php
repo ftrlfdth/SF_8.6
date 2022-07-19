68dc67d0ceea798a99f2dba97b398aec615cacc4
ftrlfdth <futumika@gmail.com>
2022-07-19 13:13:23 +0300

    сделал горизонтальным меню

--- menu.inc.php
+++ menu.inc.php
@@ -1,8 +1,8 @@
 <nav class="menu">
-	<ul>
-		<li><a href="#" class="links"><h3>About me</h3></a></li> <br>
-		<li><a href="#" class="links"><h3>feedback</h3></a></li> <br>
-		<li><a href="#" class="links"><h3>links</h3></a></li> <br>
-		<li><a href="#" class="links"><h3>contacts</h3></a></li> <br>
+	<ul class="list">
+		<li class="elements"><a href="#" class="links"><h3>About me</h3></a></li> <br>
+		<li class="elements"><a href="#" class="links"><h3>feedback</h3></a></li> <br>
+		<li class="elements"><a href="#" class="links"><h3>links</h3></a></li> <br>
+		<li class="elements"><a href="#" class="links"><h3>contacts</h3></a></li> <br>
 	</ul>
 </nav>
